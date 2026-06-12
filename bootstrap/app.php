10ac854e4b1e6b3e6c79c7f575da1298f6058d20
J9brou <j.ortizcorrea03@gmail.com>
2026-06-12 17:15:29 -0400

    fix: confiar en proxies de Laravel Cloud para URLs firmadas HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
 
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
